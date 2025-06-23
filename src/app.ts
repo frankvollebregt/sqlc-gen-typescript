fb4fdf8e453441413182dd7df18b499a44654188
Frank Vollebregt <fvollebregt@evo-it.nl>
2025-06-23 10:19:01 +0200

    Add .g. in the model file path to denote it's a generated file

--- src/app.ts
+++ src/app.ts
@@ -127,7 +127,7 @@ function codegen(input: GenerateRequest): GenerateResponse {
 
   files.push(
     new File({
-      name: "model.ts",
+      name: "model.g.ts",
       contents: new TextEncoder().encode(printNode(tableNodes)),
     })
   );
